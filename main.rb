d049587ea54aa3b38645956a0a089c443bc215d3
alexjeman <alexandrujeman.ja@gmail.com>
2020-11-17 13:23:07 +0200

    Task 3: Iterate over previously stored accounts and navigate to the page with their transactions and save those transactions data

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require_relative 'helpers'
 
 # Scraper control class, all Accounts and Transactions methods are accessed through this class
 class Scraper
-  attr_accessor :browser, :login, :password, :account_data
+  attr_accessor :browser, :login, :password, :account_data, :transaction_data
 
   def initialize(**kwargs)
     # Init browser driver, as argument or default :chrome
@@ -11,6 +11,7 @@ class Scraper
     # Optionally you can pass :login and :password kwargs if you want to login into a real account
     @browser = Watir::Browser.new(kwargs.fetch(:browser, :chrome))
     @account_data = Accounts.new
+    @transaction_data = Transactions.new
     @login = kwargs.fetch(:login, 'testaccount')
     @password = kwargs.fetch(:password, 'testpassword')
   end
@@ -49,12 +50,57 @@ class Scraper
       @account_data.add_account(name: name, currency: currency, balance: balance)
     end
   end
+
+  def fetch_transaction_data(**kwargs)
+    accounts = @account_data.get_accounts(kwargs)
+    accounts.each do |account|
+      account_name = account.fetch('name')
+      @browser.goto "https://demo.bank-on-line.ru/#Contracts/#{account_name}/Transactions"
+      date_picker(browser_instance: @browser, date: '2020-7-16')
+      @browser.span(id: 'getTranz').click
+      table_data = []
+      @browser.table(class: 'cp-tran-with-balance').wait_until_present
+      @browser.table(class: 'cp-tran-with-balance').tbody.trs.each do |tr|
+        if tr.class_name.include? 'cp-item cp-transaction' and not tr.class_name.include? 'cp-income'
+          transaction = ['withdraw']
+          tr.each { |td| transaction.append(td.text) unless td.text.empty? }
+          table_data.append(transaction)
+        elsif tr.class_name.include? 'cp-income'
+          transaction = ['deposit']
+          tr.each { |td| transaction.append(td.text) unless td.text.empty? }
+          table_data.append(transaction)
+        end
+      end
+      table_data.each do |row|
+        date = row[-3]
+        description = row[3]
+        amount = row[-2].delete(' ').to_i
+        amount = -amount if row.first.include? 'withdraw'
+        currency = symbol_to_short(row[-2][-1])
+        @transaction_data.add_transaction(date: date, description: description,
+                                          amount: amount, currency: currency,
+                                          account_name: account_name)
+      end
+    end
+  end
 end
 
-scraper = Scraper.new
 
-scraper.login_demo_account
-scraper.fetch_account_data
-scraper.account_data.print_json_account_data
-scraper.account_data.save_to_file
-sleep(999)
+
+def main
+  scraper = Scraper.new
+  scraper.login_demo_account
+  scraper.account_data.instance_reset
+  scraper.transaction_data.instance_reset
+  scraper.fetch_account_data
+  scraper.account_data.print_json_account_data
+  scraper.account_data.save_to_file
+  scraper.fetch_transaction_data
+  scraper.transaction_data.print_json_transaction_data
+  scraper.transaction_data.save_to_file
+  sleep(999)
+end
+
+if __FILE__ == $0
+  main
+end
